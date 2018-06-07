020c185a621b1d68f0ee6cfe42ca45dfb3ff5bfa
pi3rrot <4dakto@gmail.com>
2018-06-07 22:49:26 +0900

    Compose Multiple Higher Order Components Together using Recompose

--- src/App.js
+++ src/App.js
@@ -1,30 +1,58 @@
-import React, { Component, Fragment } from 'react';
+import React, { Component, Fragment } from "react";
+import { compose, setDisplayName, setPropTypes } from "recompose";
+import PropTypes from "prop-types";
+import { render } from "react-dom";
 import "./index.css";
+/*
+TITLE: Compose Multiple Higher Order Components Together using Recompose
 
-const neverRender = (BaseComponent) =>
-  class extends Component {
-    shouldComponentUpdate() {
-      return false;
-    }
-    render() {
-      return <BaseComponent {...this.props} />
-    }
-  }
+DESCRIPTION: Learn how to use the 'compose' function to mix together
+higher-order components, even ones from other libraries like 'connect'
+from redux.
+*/
 
-const User = ({ name }) =>
-  <div className="User">{ name }</div>
+const { connect } = Redux();
 
-const User2 = neverRender(User);
+const enhance = compose(
+  setDisplayName('User'),
+  setPropTypes({
+    name: PropTypes.string.isRequired,
+    status: PropTypes.string.isRequired,
+  }),
+  connect()
+);
+
+const User = enhance(({ name, status, dispatch }) =>
+  <div className="User"
+       onClick={
+         () => dispatch({ type: "USER_SELECTED"})
+       }
+  >
+    { name }: { status }
+  </div>
+);
+
+console.log(User.displayName);
 
 class App extends Component {
   render() {
     return (
       <Fragment>
-        <User name="Tim" />
-        <User2 name="Hello" />
+        <User name="Tim" status="active" />
       </Fragment>
     );
   }
 }
 
 export default App;
+
+// fake implementation of redux
+function Redux() {
+  return {
+    connect: () => (BaseComponent) => (props) =>
+      <BaseComponent
+        {...props}
+        dispatch={ ({ type }) => console.log(type + ' dispatched')}
+      />
+  }
+}
