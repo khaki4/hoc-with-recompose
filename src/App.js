4334bfcdd296f2ee004f42201e3ac3011bd33966
pi3rrot <4dakto@gmail.com>
2018-06-07 23:16:20 +0900

    Add Local State to a Functional Stateless Component using Recompose

--- src/App.js
+++ src/App.js
@@ -1,58 +1,79 @@
-import React, { Component, Fragment } from "react";
-import { compose, setDisplayName, setPropTypes } from "recompose";
-import PropTypes from "prop-types";
+import React from "react";
 import { render } from "react-dom";
-import "./index.css";
+import { compose, withReducer, withHandlers } from "recompose";
+import './index.css';
+
 /*
-TITLE: Compose Multiple Higher Order Components Together using Recompose
+TITLE:
+Add Local State to a Functional Stateless Component using Recompose
 
-DESCRIPTION: Learn how to use the 'compose' function to mix together
-higher-order components, even ones from other libraries like 'connect'
-from redux.
+DESCRIPTION:
+Learn how to use the 'withState' and 'withHandlers' higher order
+components to easily add local state to your functional stateless
+components. No need for classes!
 */
+const { Component } = React;
 
-const { connect } = Redux();
-
-const enhance = compose(
-  setDisplayName('User'),
-  setPropTypes({
-    name: PropTypes.string.isRequired,
-    status: PropTypes.string.isRequired,
-  }),
-  connect()
+const withToggle = compose(
+  withReducer(
+    "toggledOn",
+    "dispatch",
+    (state, action) => {
+      switch (action.type) {
+        case "SHOW":
+          return true;
+        case "HIDE":
+          return false;
+        case "TOGGLE":
+          return !state;
+        default:
+          return state;
+      }
+    },
+    false
+  ),
+  withHandlers({
+    show: ({ dispatch }) => e => dispatch({ type: "SHOW" }),
+    hide: ({ dispatch }) => e => dispatch({ type: "HIDE" }),
+    toggle: ({ dispatch }) => e => dispatch({ type: "TOGGLE" })
+  })
 );
 
-const User = enhance(({ name, status, dispatch }) =>
-  <div className="User"
-       onClick={
-         () => dispatch({ type: "USER_SELECTED"})
-       }
-  >
-    { name }: { status }
+const StatusList = () => (
+  <div className="StatusList">
+    <div>pending</div>
+    <div>inactive</div>
+    <div>active</div>
   </div>
 );
 
-console.log(User.displayName);
+const Status = withToggle(({ status, toggledOn, toggle }) => (
+  <span onClick={toggle}>
+    {status}
+    {toggledOn && <StatusList />}
+  </span>
+));
 
-class App extends Component {
-  render() {
-    return (
-      <Fragment>
-        <User name="Tim" status="active" />
-      </Fragment>
-    );
-  }
-}
+const Tooltip = withToggle(({ text, children, toggledOn, show, hide }) => (
+  <span>
+    {toggledOn && <div className="Tooltip">{text}</div>}
+    <span onMouseEnter={show} onMouseLeave={hide}>
+      {children}
+    </span>
+  </span>
+));
+
+const User = ({ name, status }) => (
+  <div className="User">
+    <Tooltip text="Cool Dude!">{name}</Tooltip>—
+    <Status status={status} />
+  </div>
+);
+
+const App = () => (
+  <div>
+    <User name="Tim" status="active" />
+  </div>
+);
 
 export default App;
-
-// fake implementation of redux
-function Redux() {
-  return {
-    connect: () => (BaseComponent) => (props) =>
-      <BaseComponent
-        {...props}
-        dispatch={ ({ type }) => console.log(type + ' dispatched')}
-      />
-  }
-}
