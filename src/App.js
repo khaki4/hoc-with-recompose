5ab248e225f108322088662e5d61fac18f1a1c90
Jiwon Jung <jw.jeoung@albamapp.com>
2018-06-08 11:09:00 +0900

    Lock Props by using the Recompose withProps Higher Order Component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { mapProps, lifecycle } from "recompose";
+import { withProps } from "recompose";
 import './index.css';
 
 /*
@@ -10,52 +10,24 @@ DESCRIPTION:
 Learn how to use the ‘withProps’ higher order component to pre-fill a prop,
 unable to be overridden.
 */
+const { Component } = React;
 
-const User = ({ name, status }) =>
-  <div className="User">{name}—{status}</div>;
+const HomeLink = withProps(({ query }) => ({ href: "#/?query=" + query }))("a");
+const ProductsLink = withProps({ href: "#/products" })("a");
+const CheckoutLink = withProps({ href: "#/checkout" })("a");
 
-
-const UserList = ({ users, status }) =>
-  <div className="UserList">
-    <h3>{ status } users</h3>
-    { users && users.map((user) => <User {...user} />)}
-  </div>
-
-const users = [
-  { name: "Tim", status: 'active' },
-  { name: "Bob", status: 'pending' },
-  { name: "Joe", status: 'active' },
-  { name: "Jim", status: 'inactive' },
-];
-
-const filterByStatus = (status) => mapProps(
-  ({ users }) => {
-    return {
-      status,
-      users: users.filter(u => u.status === status)
-    }
-  }
-);
-
-const ActiveUsers = filterByStatus('active')(UserList);
-const InactiveUsers = filterByStatus('inactive')(UserList);
-const PendingUsers = filterByStatus('pending')(UserList);
-
-const App = () =>
+const App = () => (
   <div className="App">
-    {/*<h3>active users</h3>*/}
-    {/*<UserList users={users.filter(u => u.status === 'active')} />*/}
-
-    {/*<h3>inactive users</h3>*/}
-    {/*<UserList users={users.filter(u => u.status === 'inactive')} />*/}
-
-    {/*<h3>pending users</h3>*/}
-    {/*<UserList users={users.filter(u => u.status === 'pending')} />*/}
-    <ActiveUsers users={ users } />
-    <InactiveUsers users={ users } />
-    <PendingUsers users={ users } />
-  </div>;
-
+    <header>
+      <HomeLink query="logo">Logo</HomeLink>
+    </header>
+    <nav>
+      <HomeLink>Home</HomeLink>
+      <ProductsLink>Products</ProductsLink>
+      <CheckoutLink>Checkout</CheckoutLink>
+    </nav>
+  </div>
+);
 
 
 export default App;
