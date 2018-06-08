e47521df91d64acb95694ec4ac500dfb27b5d35a
Jiwon Jung <jw.jeoung@albamapp.com>
2018-06-08 10:58:31 +0900

    Transform Props by using the Recompose mapProps Higher Order Component

--- src/App.js
+++ src/App.js
@@ -1,60 +1,62 @@
-import React, { Component } from "react";
-import { render } from "react-dom";
+import React from "react";
 import { mapProps, lifecycle } from "recompose";
 import './index.css';
+
 /*
 TITLE:
-Transform Props using Recompose
+Lock Props using Recompose
 
 DESCRIPTION:
-Learn how to use the 'mapProps' higher-order component to modify an
-existing component’s API (its props). 'mapProps' takes incoming props
-and changes them however you’d like; for example, filtering the props
-by a field.
+Learn how to use the ‘withProps’ higher order component to pre-fill a prop,
+unable to be overridden.
 */
 
-const configPromise = fetchConfiguration();
-
-const withConfig = lifecycle({
-  state: { config: {} },
-  componentDidMount() {
-    configPromise.then(config => this.setState({ config }));
-  }
-});
+const User = ({ name, status }) =>
+  <div className="User">{name}—{status}</div>;
 
 
-
-const User = withConfig(({ name, status, config }) => {
-   return (
-     <div className="User">
-       { name }
-       { config.showStatus && '-' + status }
-       { config.canDeleteUsers && <button>x</button> }
-     </div>
-   );
-  }
-);
-
-const App = () => (
-  <div>
-    <User
-      name="Tim"
-      status="actives"
-      showStatus
-      canDleteUsers
-    />
+const UserList = ({ users, status }) =>
+  <div className="UserList">
+    <h3>{ status } users</h3>
+    { users && users.map((user) => <User {...user} />)}
   </div>
+
+const users = [
+  { name: "Tim", status: 'active' },
+  { name: "Bob", status: 'pending' },
+  { name: "Joe", status: 'active' },
+  { name: "Jim", status: 'inactive' },
+];
+
+const filterByStatus = (status) => mapProps(
+  ({ users }) => {
+    return {
+      status,
+      users: users.filter(u => u.status === status)
+    }
+  }
 );
+
+const ActiveUsers = filterByStatus('active')(UserList);
+const InactiveUsers = filterByStatus('inactive')(UserList);
+const PendingUsers = filterByStatus('pending')(UserList);
+
+const App = () =>
+  <div className="App">
+    {/*<h3>active users</h3>*/}
+    {/*<UserList users={users.filter(u => u.status === 'active')} />*/}
+
+    {/*<h3>inactive users</h3>*/}
+    {/*<UserList users={users.filter(u => u.status === 'inactive')} />*/}
+
+    {/*<h3>pending users</h3>*/}
+    {/*<UserList users={users.filter(u => u.status === 'pending')} />*/}
+    <ActiveUsers users={ users } />
+    <InactiveUsers users={ users } />
+    <PendingUsers users={ users } />
+  </div>;
+
+
 
 export default App;
 
-const config = {
-  showStatus: true,
-  canDeleteUsers: true
-}
-
-function fetchConfiguration() {
-  return new Promise((resolve) => {
-    setTimeout(() => resolve(config), 300);
-  });
-}
